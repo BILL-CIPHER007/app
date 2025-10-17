522b81204a8b2843cb2546bd336adf2b388ca8b3
BILL-CIPHER007 <mateusrodrigue3425@gmail.com>
2025-10-17 19:20:24 -0300

    mud next config

--- next.config.js
+++ next.config.js
@@ -2,16 +2,20 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  outputFileTracingRoot: path.join(__dirname, '../'),
+  distDir: '.next',
+
+  // ✅ Corrigido: apontando para o diretório atual do projeto
+  outputFileTracingRoot: path.join(__dirname),
+
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: false,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
